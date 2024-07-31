c9aa23eb38bb459e0869d25d5c145a6a536e35ef
Carlos <tiagocdz29@hotmail.com>
2024-07-30 21:18:46 -0400

    Implementação dos botoes

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: Color.fromARGB(255, 4, 97, 4), useMaterial3: true),
+      theme: ThemeData(colorSchemeSeed: Colors.green[800], useMaterial3: true),
       home: const AppBarExample(),
     );
   }
@@ -41,20 +40,9 @@ class AppBarExample extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('DETRAN MS'),
-          // This check specifies which nested Scrollable's scroll notification
-          // should be listened to.
-          //
-          // When `ThemeData.useMaterial3` is true and scroll view has
-          // scrolled underneath the app bar, this updates the app bar
-          // background color and elevation.
-          //
-          // This sets `notification.depth == 1` to listen to the scroll
-          // notification from the nested `ListView.builder`.
           notificationPredicate: (ScrollNotification notification) {
             return notification.depth == 1;
           },
-          // The elevation value of the app bar when scroll view has
-          // scrolled underneath the app bar.
           scrolledUnderElevation: 4.0,
           shadowColor: Theme.of(context).shadowColor,
           bottom: const TabBar(
@@ -76,34 +64,67 @@ class AppBarExample extends StatelessWidget {
         ),
         body: TabBarView(
           children: <Widget>[
-            const Center(
+            Center(
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Menu Habilitação do DETRAN - MS',
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextField(
-                        //controller: _placaController,
-                        decoration: InputDecoration(
-                          labelText: 'Placa',
-                          border: OutlineInputBorder(),
-                        ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'Menu Habilitação do DETRAN - MS',
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      //controller: _placaController,
+                      decoration: InputDecoration(
+                        labelText: 'Placa',
+                        border: OutlineInputBorder(),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: TextField(
-                        // controller: _renavamController,
-                        decoration: InputDecoration(
-                          labelText: 'RENAVAM',
-                          border: OutlineInputBorder(),
-                        ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: TextField(
+                      // controller: _renavamController,
+                      decoration: InputDecoration(
+                        labelText: 'RENAVAM',
+                        border: OutlineInputBorder(),
                       ),
                     ),
-                  ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação ao pressionar o botão 1
+                            print('Botão de Enviar pressionado!');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[200],
+                            foregroundColor: Colors.green[800],
+                          ),
+                          child: const Text('Cancelar'),
+                        ),
+                        const SizedBox(
+                            width: 16), // Espaçamento entre os botões
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação ao pressionar o botão 2
+                            print('Botão de Cancelar pressionado!');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green[800],
+                            foregroundColor: Colors.white,
+                          ),
+                          child: const Text('Enviar'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             //ListView.builder(
             Row(
@@ -128,14 +149,6 @@ class AppBarExample extends StatelessWidget {
                 ),
               ],
             ),
-            //itemCount: 25,
-            //itemBuilder: (BuildContext context, int index) {
-            // return ListTile(
-            //     tileColor: index.isOdd ? oddItemColor : evenItemColor,
-            //     title: Text('${titles[1]} $index'),
-            //     );
-            //},
-            // ),
             ListView.builder(
               itemCount: 25,
               itemBuilder: (BuildContext context, int index) {
@@ -151,146 +164,3 @@ class AppBarExample extends StatelessWidget {
     );
   }
 }
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-//         useMaterial3: true,
-//       ),
-//       home: Container(
-//         color: Colors.white,
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: [
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.white,
-//                   width: 50,
-//                   height: 50,
-//                 ),
-//               ],
-//             ),
-//             Stack(
-//               alignment: AlignmentDirectional.center,
-//               children: [
-//                 Container(
-//                   color: Colors.white,
-//                   width: 100,
-//                   height: 100,
-//                 ),
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 50,
-//                   height: 50,
-//                 ),
-//               ],
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               crossAxisAlignment: CrossAxisAlignment.center,
-//               children: [
-//                 Container(
-//                   color: Colors.cyan,
-//                   width: 50,
-//                   height: 50,
-//                 ),
-//                 Container(
-//                   color: Colors.pinkAccent,
-//                   width: 50,
-//                   height: 50,
-//                 ),
-//                 Container(
-//                   color: Colors.purple,
-//                   width: 50,
-//                   height: 50,
-//                 ),
-//               ],
-//             ),
-//             Container(
-//               color: Colors.amber.shade700,
-//               width: 300,
-//               height: 30,
-//               child: const Text(
-//                 'Diamante amarelo',
-//                 style: TextStyle(
-//                   color: Colors.black,
-//                   fontSize: 28,
-//                 ),
-//                 textAlign: TextAlign.center,
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 print('Você apertou o botão');
-//               },
-//               child: Text('Entrar'),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
- // Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   children: [
-      //     Container(
-      //       color: Colors.white,
-      //       width: 100,
-      //       height: 100,
-      //     ),
-      //     Container(
-      //       color: Colors.blue,
-      //       width: 100,
-      //       height: 100,
-      //     ),
-      //   ],
-      // ),
-      // Column(
-      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   children: [
-      //     Container(
-      //       color: Colors.white,
-      //       width: 100,
-      //       height: 100,
-      //     ),
-      //     Container(
-      //       color: Colors.blue,
-      //       width: 100,
-      //       height: 100,
-      //     ),
-      //   ],
-      // ),
-      // Stack(
-      //   alignment: AlignmentDirectional.topCenter,
-      //   children: [
-      //     Container(
-      //       color: Colors.white,
-      //     ),
-      //     Container(
-      //       color: Colors.blue,
-      //       width: 500,
-      //       height: 80,
-      //     ),
-      //   ],
-      // ),
